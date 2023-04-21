5298838defdc1fc84fd8ec0e6358512b8a0b03ff
Намазов Эмин Ильгар оглы <ENamazov@cbgr.ru>
2023-04-21 16:20:31 +0300

    new implementation ipV6Support

--- src/main/java/com/example/accesskeybackend/template/controller/IPVersionPublicController.java
+++ src/main/java/com/example/accesskeybackend/template/controller/IPVersionPublicController.java
@@ -1,11 +1,14 @@
 package com.example.accesskeybackend.template.controller;
 
+import java.util.Collections;
+import java.util.Map;
+
 import com.example.accesskeybackend.template.service.IPVersionService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -17,8 +20,8 @@ public class IPVersionPublicController {
 
     private final IPVersionService ipVersionService;
 
-    @GetMapping("/{siteUrl}")
-    public ResponseEntity<Boolean> checkIpv6ByUrl(@PathVariable String siteUrl) {
-        return ResponseEntity.ok(ipVersionService.checkIpv6Support(siteUrl));
+    @GetMapping
+    public ResponseEntity<Map<String, Boolean>> checkIpv6ByUrl(@RequestParam("url") String siteUrl) {
+        return ResponseEntity.ok(Collections.singletonMap("success", ipVersionService.checkIpv6SupportV1(siteUrl)));
     }
 }
--- src/main/java/com/example/accesskeybackend/template/service/IPVersionService.java
+++ src/main/java/com/example/accesskeybackend/template/service/IPVersionService.java
@@ -1,24 +1,47 @@
 package com.example.accesskeybackend.template.service;
 
 import java.net.InetAddress;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.UnknownHostException;
 import java.util.regex.Pattern;
+import org.apache.commons.validator.routines.InetAddressValidator;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class IPVersionService {
 
-    public boolean checkIpv6Support(String siteUrl) {
+    public boolean checkIpv6SupportV1(String siteUrl) {
         try {
+            String address;
+            if (siteUrl.contains(":")) {
+                URI uri = new URI(siteUrl);
+                String host = uri.getHost();
+                address = InetAddress.getByName(host).getHostAddress();
+            } else {
+                address = InetAddress.getByName(siteUrl).getHostAddress();
+            }
             return Pattern.compile("^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$")
-                    .matcher(InetAddress
-                            .getByName(new URL(siteUrl).getHost())
-                            .getHostAddress())
+                    .matcher(address)
                     .find();
-        } catch (UnknownHostException | MalformedURLException e) {
+        } catch (URISyntaxException | UnknownHostException e) {
+            throw new IllegalArgumentException("Cant' check for IPv6 support this url: " + siteUrl);
+        }
+    }
+
+    public boolean checkIpv6SupportV2(String siteUrl) {
+        try {
+            String address;
+            if (siteUrl.contains(":")) {
+                URI uri = new URI(siteUrl);
+                String host = uri.getHost();
+                address = InetAddress.getByName(host).getHostAddress();
+            } else {
+                address = InetAddress.getByName(siteUrl).getHostAddress();
+            }
+            return InetAddressValidator.getInstance().isValidInet6Address(address);
+        } catch (URISyntaxException | UnknownHostException e) {
             throw new IllegalArgumentException("Cant' check for IPv6 support this url: " + siteUrl);
         }
     }
